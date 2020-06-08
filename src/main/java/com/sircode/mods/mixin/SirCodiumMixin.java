49579b48bbcc0aeae451ce030124123dfe35bbd0
Sir Code <taylorschneider07@yahoo.com>
2020-06-08 18:51:19 -0400

    Update SirCodiumMixin.java

--- src/main/java/com/sircode/mods/mixin/SirCodiumMixin.java
+++ src/main/java/com/sircode/mods/mixin/SirCodiumMixin.java
@@ -1,15 +1,17 @@
 package com.sircode.mods.mixin;
 
+import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(TitleScreen.class)
+@Mixin(ChatScreen.class)
 public class SirCodiumMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+	@Inject(method = "keyPressed(III)Z", at = @At("RETURN"), cancellable = true)
+	public void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+		if(keyCode > 10) cir.setReturnValue(true);
 	}
 }
